a78809ad1dd42011e60316ffed13bc8da65f76a6
Gayathri Geetha Nair <gayathri@Gayathris-MacBook-Air.local>
2025-03-19 11:03:26 +0000

    fixing port for hosting

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from app import create_app
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-
+import os
 
 
 app = create_app()
@@ -12,5 +12,6 @@ jwt = JWTManager(app)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT",4000))
+    app.run(host="0.0.0.0", port=port)
 
